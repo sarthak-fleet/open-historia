4e11fef78cf3f9adc54877d04cfa9a199921f58f
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-06-04 21:17:13 +0530

    eslint: ignore landing-astro/

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import nextConfig from "eslint-config-next";
 
 export default [
-  { ignores: ["dist", ".next", "build", ".wrangler", "node_modules", "out", ".open-next"] },
+  { ignores: ["dist", "landing-astro", ".next", "build", ".wrangler", "node_modules", "out", ".open-next"] },
   ...nextConfig,
 ];
